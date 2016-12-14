d815796ba373d6f7c0bce2091d73dea912978bab
Chris Richards <chris.richards@yellowfeather.co.uk>
2016-12-14 15:20:04 +0000

    Removes Invoke-MSBuild step

--- Build.ps1
+++ Build.ps1
@@ -54,8 +54,6 @@ EnsurePsbuildInstalled
 
 exec { & dotnet restore }
 
-Invoke-MSBuild
-
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
 
